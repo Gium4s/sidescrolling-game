822aa8837b0ae41833ec41919cc405c594e9c5ac
Gium4s <giumas2004@gmail.com>
2025-12-29 15:10:11 +0100

    Update UI.ts

---
+++ src/scenes/UI.ts
@@ -0,0 +1,10 @@
+import Phaser from 'phaser'
+import { sharedInstance as events } from './EventCenter'
+
+export default class UI extends Phaser.Scene {
+    constructor() {
+        super('ui')
+    }
+
+    // ...il resto del tuo codice...
+}
